7d76633b478bf0186900e03477a321292d9da54e
PradeepReddy10102020 <pradeep.kadthala@gmail.com>
2024-10-23 08:54:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Cals from './Components/cals.js';
+
+function App() {
+  return (
+ <div>
+<Cals>
+  <h1>calculater</h1>
+</Cals>
+ </div>
+  );
+}
+
+export default App;
